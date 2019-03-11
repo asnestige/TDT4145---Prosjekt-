9f39e5bfca239750f1767219a839c6ca7281f374
Jenny Yu <jennyjinyiyu@gmail.com>
2019-03-11 12:59:29 +0100

    added example files

---
+++ Eksempel/DBConn.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Svein Erik
+ */
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public abstract class DBConn {
+    protected Connection conn;
+    public DBConn () {
+    }
+    public void connect() {
+    	try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            // Properties for user and password. Here the user and password are both 'paulr'
+            Properties p = new Properties();
+            p.put("user", "jennyjy_db");
+            p.put("password", "yulve123");
+	    //            conn = DriverManager.getConnection("jdbc:mysql://mysql.ansatt.ntnu.no/sveinbra_ektdb?autoReconnect=true&useSSL=false",p);
+            conn = DriverManager.getConnection("jdbc:mysql://mysql.stud.ntnu.no/heddasu_dbkissa/ekt?autoReconnect=true&useSSL=false",p);
+        } catch (Exception e)
+    	{
+            throw new RuntimeException("Unable to connect", e);
+    	}
+    }
+}
