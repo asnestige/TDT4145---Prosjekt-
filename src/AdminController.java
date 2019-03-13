654f24f6b071b6f433b07212ba1b01987f261c58
Jenny Yu <jennyjinyiyu@gmail.com>
2019-03-13 15:29:21 +0100

    kravspesifikasjon 2 jobbes med

--- src/AdminController.java
+++ src/AdminController.java
@@ -13,7 +13,7 @@ import java.sql.Timestamp;
 
 public class AdminController {
 
-    ///////KRAV SPEK 1 //////////
+    //1. Registrere apparater, øvelser og treningsøkter med tilhørende data.
 
     public static void settInnTreningsokt(Connection conn, Timestamp tidsstempel, int varighet, int form, int prestasjon, String notat) throws SQLException {
         String preQueryStatement = "INSERT INTO treningsøkt (Tidsstempel, Varighet, Form, Prestasjon, Notat) values (?,?,?,?,?)";
@@ -44,6 +44,31 @@ public class AdminController {
         prepState.setInt(1, apparatID);
         prepState.setString(2, apparatNavn);
         prepState.execute();
+    }
+
+
+
+    // 2. Få opp informasjon om et antall n sist gjennomførte treningsøkter med notater, der n
+    //spesifiseres av brukeren.
+
+    public static List<Treningsokt> hentOkter(Connection conn, int n) throws SQLException {
+        List<Treningsokt> okter = new ArrayList<Treningsokt>();
+
+        String preQueryStatement = "SELECT * FROM treningsøkt ORDER BY Tidsstempel DESC LIMIT ?";
+        //preQueryStatement += n;
+        PreparedStatement prepState = conn.prepareStatement(preQueryStatement);
+
+        prepState.setInt(1,n);
+        ResultSet rs = prepState.executeQuery();
+
+        while (rs.next()) {
+            Treningsokt okt = new Treningsokt(rs.getTimestamp("Tidsstempel"), rs.getInt("Varighet"), rs.getInt("Form"),
+                    rs.getInt("Prestasjon"), rs.getString("Notat"));
+            okter.add(okt);
+
+        }
+        prepState.execute();
+        return okter;
     }
 
 }
